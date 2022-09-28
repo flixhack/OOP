1f6304518edffd11d3b58a97f9d1138406f1fb3b
Pætur Magnussen <55387402+pmagnu@users.noreply.github.com>
2022-09-28 13:53:03 +0200

    :)
    lektion 3 opgave løsninger ????

---
+++ lektion_3/opgave1/opgave1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace opgave1
+{
+
+    class CarComparer : IComparer<Car>
+    {
+        public int Compare(Car? x, Car? y)
+        {
+            if (x.Make.CompareTo(y.Make) == 0)
+            {
+                if (x.Model.CompareTo(y.Model) == 0)
+                {
+                    if (x.Price > y.Price)
+                    {
+                        return 1;
+                    }
+                    else if (x.Price < y.Price)
+                    {
+                        return -1;
+                    }
+                    else return 0;
+                }
+                else if (x.Model.CompareTo(y.Model) > 0)
+                {
+                    return 1;
+                }
+                else return -1;
+            }
+            if (x.Make.CompareTo(y.Make) > 0)
+            {
+                return 1;
+            }
+            else return -1;
+        }
+
+    }
+    class Car : IComparable<Car>
+    {
+        public string Make { get; private set; }
+        public string Model { get; private set; }
+        public decimal Price { get; private set; }
+        public Car(string make, string model, decimal price)
+        {
+            Make = make;
+            Model = model;
+            Price = price;
+        }
+
+        
+        public int CompareTo(Car? other)
+        {
+            if (Price > other.Price)
+            {
+                return -1;
+            }
+            if (Price < other.Price)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>()
+            {
+                new Car("Skoda", "Fabia", 50000m),
+                new Car("Skoda", "Octavia", 60000m),
+                new Car("Fiat", "500", 12345m),
+                new Car("Ford", "Mustang", 9000000m),
+                new Car("Ford", "Mustang", 9000001m)
+             };
+                        
+            cars.Sort(new CarComparer());
+            Console.WriteLine("Sorted by price");
+            foreach (Car car in cars)
+            {
+                Console.WriteLine($" {car.Make} {car.Model} {car.Price}");
+            }
+            Console.Read();
+        }
+    }
+
+}
---
+++ lektion_3/opgave2/opgave2/opgave2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+
+namespace opgave2
+{
+    public interface ITaxable
+    {
+        decimal TaxValue { get;}
+    }
+    public class FixedProperty
+    {
+        protected string location;
+        protected bool inCity;
+        protected decimal estimatedValue;
+
+        public FixedProperty(string location, bool inCity, decimal value)
+        {
+            this.location = location;
+            this.inCity = inCity;
+            this.estimatedValue = value;
+        }
+
+        public FixedProperty(string location) :
+          this(location, true, 1000000)
+        {
+        }
+
+        public string Location
+        {
+            get
+            {
+                return location;
+            }
+        }
+    }
+
+    public class Vehicle
+    {
+        protected int registrationNumber;
+        protected double maxVelocity;
+        protected decimal value;
+
+        public Vehicle(int registrationNumber, double maxVelocity, decimal value)
+        {
+            this.registrationNumber = registrationNumber;
+            this.maxVelocity = maxVelocity;
+            this.value = value;
+        }
+
+        public int RegistrationNumber
+        {
+            get
+            {
+                return registrationNumber;
+            }
+        }
+
+    }
+
+    public class Bus : Vehicle, ITaxable
+    {
+        protected int numberOfSeats;
+        public Bus(int numberOfSeats, int regNumber, decimal value) :
+            base(regNumber, 80, value)
+        {
+            this.numberOfSeats = numberOfSeats;
+        }
+        public int NumberOfSeats
+        {
+            get
+            {
+                return numberOfSeats;
+            }
+        }
+
+        decimal ITaxable.TaxValue => 6969;
+    }
+
+    public class House : FixedProperty, ITaxable
+    {
+        protected double area;
+        public decimal TaxValue { get; }
+
+        public House(string location, bool inCity, double area, decimal value) :
+            base(location, inCity, value)
+        {
+            this.area = area;
+            this.TaxValue = value/(decimal)area;
+        }
+
+        public double Area
+        {
+            get
+            {
+                return area;
+            }
+        }
+
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            FixedProperty property = new FixedProperty("Himmerlandsgad", true, 1000);
+            Vehicle vehicle = new Bus(1234, 100, 2000);
+
+            ITaxable bus = new Bus(59, 1111, 20000);
+            ITaxable house = new House("aalborg", true, 90, 500000);
+
+            Console.WriteLine("Taxvalue for bus: " + bus.TaxValue + ", and house: " + house.TaxValue);
+            Console.Read();
+        }
+    }
+}
